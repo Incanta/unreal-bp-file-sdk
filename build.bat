745dd6f5a8164be4b7c07dfcb53e1c180c4a19ec
Mike Seese <seesemichaelj@gmail.com>
2020-12-05 23:37:41 -0800

    update to 4.26

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 echo off
 
-set LINUX_MULTIARCH_ROOT=C:\UnrealToolchains\v16_clang-9.0.1-centos7\
+set LINUX_MULTIARCH_ROOT=C:\UnrealToolchains\v17_clang-10.0.1-centos7\
 %LINUX_MULTIARCH_ROOT%x86_64-unknown-linux-gnu\bin\clang++ -v
 
-"C:\Program Files\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" ^
+"C:\Program Files\Epic Games\UE_4.26\Engine\Build\BatchFiles\RunUAT.bat" ^
   BuildPlugin ^
   -Plugin=%CD%\FileSDK.uplugin ^
   -Package=%CD%\Dist ^
