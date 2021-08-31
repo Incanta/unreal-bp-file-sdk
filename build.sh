865415d555c1916c7524cd268206cbba894cadfe
Mike Seese <seesemichaelj@gmail.com>
2021-08-30 22:12:43 -0700

    upgrade to 4.27

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 # only used in windows for cross compiling
-export LINUX_MULTIARCH_ROOT=/c/UnrealToolchains/v17_clang-10.0.1-centos7/
+export LINUX_MULTIARCH_ROOT=/c/UnrealToolchains/v19_clang-11.0.1-centos7/
 ${LINUX_MULTIARCH_ROOT}x86_64-unknown-linux-gnu/bin/clang++ -v
 
+export ENGINE_VERSION=4.27
+
 if [ "$(uname)" = "Darwin" ]; then
-  UAT_PATH="/Users/Shared/Epic Games/UE_4.26/Engine/Build/BatchFiles/RunUAT.sh"
+  UAT_PATH="/Users/Shared/Epic Games/UE_${ENGINE_VERSION}/Engine/Build/BatchFiles/RunUAT.sh"
   TARGET_PLATFORMS=Mac
 else
   # windows handles linux cross compile
-  UAT_PATH="/c/Program Files/Epic Games/UE_4.26/Engine/Build/BatchFiles/RunUAT.bat"
+  UAT_PATH="/c/Program Files/Epic Games/UE_${ENGINE_VERSION}/Engine/Build/BatchFiles/RunUAT.bat"
   TARGET_PLATFORMS=Win64+Linux
 fi
 
